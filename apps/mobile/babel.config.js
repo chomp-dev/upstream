a7b9f62fad814244025cba58027d4c6c1d3f702b
brian Li <brian3092li@gmail.com>
2026-02-05 11:12:01 -0600

    fix: force define EXPO_ROUTER_IMPORT_MODE to sync

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -5,6 +5,7 @@ module.exports = function (api) {
     plugins: [
       ['transform-define', {
         'process.env.EXPO_ROUTER_APP_ROOT': '../../app',
+        'process.env.EXPO_ROUTER_IMPORT_MODE': 'sync',
       }],
       // Required for react-native-reanimated (must be last)
       'react-native-reanimated/plugin',
