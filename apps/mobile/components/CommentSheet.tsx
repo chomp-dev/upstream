33a8afb12a81ce1d88ed2b396cebab031207f7bc
brian Li <brian3092li@gmail.com>
2026-02-04 20:43:07 -0600

    fix: add missing import

--- apps/mobile/components/CommentSheet.tsx
+++ apps/mobile/components/CommentSheet.tsx
@@ -8,6 +8,7 @@ import {
     KeyboardAvoidingView,
     Platform,
     ActivityIndicator,
+    TouchableWithoutFeedback,
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useAuth } from '../src/context/auth';
