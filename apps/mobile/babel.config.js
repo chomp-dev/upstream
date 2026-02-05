99e66b1dda64dfc8b2e0444ac61dcdfca676db9e
brian Li <brian3092li@gmail.com>
2026-02-05 11:06:55 -0600

    fix: force define EXPO_ROUTER_APP_ROOT with correct relative path

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -3,6 +3,9 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      ['transform-define', {
+        'process.env.EXPO_ROUTER_APP_ROOT': '../../app',
+      }],
       // Required for react-native-reanimated (must be last)
       'react-native-reanimated/plugin',
     ],
