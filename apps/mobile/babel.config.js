d35eb8aac57e071eab63bb77790990c66349a6f1
brian Li <brian3092li@gmail.com>
2026-02-05 10:54:45 -0600

    fix: add expo-router/babel plugin and navigation deps

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,8 +1,9 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      'expo-router/babel',
       // Required for react-native-reanimated (must be last)
       'react-native-reanimated/plugin',
     ],
